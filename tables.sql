438b499169fac773206020526ad16731c066b7f5
sfassnacht1 <51734546+sfassnacht1@users.noreply.github.com>
2019-10-13 18:45:30 -0400

    Add files via upload
    This is the SQL that creates all the tables. It's autogenerated by an Excel file with some macros.

---
+++ tables.sql
@@ -0,0 +1,73 @@
+create table Student(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+loginid varchar(15),
+username varchar(40),
+lastname varchar(40),
+firstname varchar(40),
+token varchar(40),
+deleted boolean);
+
+create table Professor(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+loginid varchar(15),
+title varchar(24),
+lastname varchar(40),
+firstname varchar(40),
+token varchar(40),
+deleted boolean);
+
+create table Course(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+courseid varchar(40),
+coursename varchar(40),
+deleted boolean);
+
+create table Class(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+courseid integer,
+professorid integer,
+section integer,
+FOREIGN KEY(professorid) references Professor(tid), 
+FOREIGN KEY(courseid) references Course(tid),,
+deleted boolean);
+
+create table Roll(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+classid integer,
+studentid integer,
+FOREIGN KEY(classid) references Class(tid), 
+FOREIGN KEY(studentid) references Student(tid),,
+deleted boolean);
+
+create table Login(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+rollid integer,
+ipaddress varchar(24),
+latitude float,
+longitude float,
+verifycode varchar(40),
+logindate datetime,
+result integer,
+token varchar(40),
+FOREIGN KEY(rollid) references Roll(tid),
+deleted boolean);
+
+create table Verify(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+classid integer,
+verifycode varchar(40),
+datetimeset datetime,
+token varchar(40),
+FOREIGN KEY(classid) references Class(tid),,
+deleted boolean);
+
+create table SetCourse(
+tid integer AUTO_INCREMENT PRIMARY KEY,
+classid integer,
+professorid integer,
+classset datetime,
+verifycode varchar(40),
+FOREIGN KEY(classid) references Class(tid), 
+FOREIGN KEY(professorid) references Professor(tid),,
+deleted boolean);
+
